c4f821ce5a40398aeffd5e22841deaaabcd75558
Kei Nakazawa <muo@muo.jp>
2017-04-12 12:48:15 +0900

    Fix chapref generator

--- articles/review-ext.rb
+++ articles/review-ext.rb
@@ -1,12 +1,8 @@
 module ReVIEW
   class HTMLBuilder
-    def id2blogger_url(id)
-      id.gsub(/^(\d{2})(\d{2})_/, '/20\1/\2/')
-    end
     def inline_chapref(id)
       title = super
-      relative_url = id2blogger_url(id)
-      %Q(<a href="#{relative_url}#{extname}">#{title}</a>)
+      %Q(<a href="/#{id}#{extname}">#{title}</a>)
     end
   end
   class Builder
